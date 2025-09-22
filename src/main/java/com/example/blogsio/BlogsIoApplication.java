f80bc58083ef3aa3cae33269f16b96e992004f1b
madtitan1712 <vasusudhan123@gmail.com>
2025-09-22 17:53:43 +0530

    Initial Commit after repo and DB setup

---
+++ src/main/java/com/example/blogsio/BlogsIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogsio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blogsio/BlogsIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogsio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
